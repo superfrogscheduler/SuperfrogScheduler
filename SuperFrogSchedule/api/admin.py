13cba10537c0f2b67d4ccd6e9b9a44bdd04e17bb
Duncan Scott Martinson <dmscottm@yahoo.com>
2018-11-27 14:33:24 -0600

    created appearance models

--- SuperFrogSchedule/api/admin.py
+++ SuperFrogSchedule/api/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
-
+from.models import Appearance, OrgType,TeamType,Event
 # Register your models here.
+admin.site.register(Appearance)
+admin.site.register(OrgType)
+admin.site.register(TeamType)
+admin.site.register(Event)
--- SuperFrogSchedule/api/models.py
+++ SuperFrogSchedule/api/models.py
@@ -1,6 +1,25 @@
 from django.db import models
 
 class Event(models.Model):
-    name = models.CharField(max_length=50)
+    name = models.CharField(max_length=255)
     date = models.DateTimeField()
 
+class Appearance(models.Model):
+    event_id = models.ForeignKey(Event, on_delete = "CASCADE")
+    organization = models.CharField(max_length = 255)
+    location = models.CharField(max_length=255)
+    parking_info = models.CharField(max_length=255, blank=True)
+    org_type = models.ForeignKey("OrgType", on_delete="NULL")
+    team_type = models.ForeignKey("TeamType", on_delete="NULL")
+    performance_required = models.BooleanField()
+    location_for_belongings = models.CharField(max_length=255, blank=True)
+    expenses_and_benefits = models.CharField(max_length=255, blank=True)
+    outside_orgs = models.BooleanField()
+    description = models.CharField(max_length = 1000)
+
+class OrgType(models.Model):
+    org_type=models.CharField(max_length = 255)
+
+class TeamType(models.Model):
+    team_type = models.CharField(max_length = 255)
+    
