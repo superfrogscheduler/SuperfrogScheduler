f6dd38a76f6eee5f44629b930f626dffdd090cfe
jclazalde <joselazalde93@yahoo.com>
2018-11-28 13:29:30 -0600

    Jose_Model and Serializers

--- SuperFrogSchedule/api/admin.py
+++ SuperFrogSchedule/api/admin.py
@@ -1,6 +1,9 @@
 from django.contrib import admin
-from.models import Appearance, OrgType,TeamType,Event
+from .models import Superfrog, Customer, Event, Admin, Appearance, OrgType, TeamType
 # Register your models here.
+admin.site.register(Superfrog)
+admin.site.register(Customer)
+admin.site.register(Admin)
 admin.site.register(Appearance)
 admin.site.register(OrgType)
 admin.site.register(TeamType)
