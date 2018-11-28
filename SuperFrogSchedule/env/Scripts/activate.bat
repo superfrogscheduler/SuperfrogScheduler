e50e4b19e30fe0cc25aa4357737e75469becd493
jclazalde <joselazalde93@yahoo.com>
2018-11-28 14:24:30 -0600

    Adding stuff

--- SuperFrogSchedule/env/Scripts/activate.bat
+++ SuperFrogSchedule/env/Scripts/activate.bat
@@ -1,5 +1,5 @@
 @echo off
-set "VIRTUAL_ENV=C:\SuperFrogScheduler\SuperFrogSchedule\env"
+set "VIRTUAL_ENV=C:\Users\josel\OneDrive\Documents\GitHub\SuperfrogScheduler\SuperFrogSchedule\env"
 
 if defined _OLD_VIRTUAL_PROMPT (
     set "PROMPT=%_OLD_VIRTUAL_PROMPT%"
