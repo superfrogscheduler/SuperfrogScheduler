f638f1dc353c74f002e0925040606b6267130e65
jclazalde <joselazalde93@yahoo.com>
2018-11-29 19:29:48 -0600

    Create accept-form.compenent.spec.ts

---
+++ frontend/src/app/accept-form/accept-form.compenent.spec.ts
@@ -0,0 +1,25 @@
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { AcceptFormComponent } from './accept-form.component';
+
+describe('AcceptFormComponent', () => {
+  let component: AcceptFormComponent;
+  let fixture: ComponentFixture<AcceptFormComponent>;
+
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [ AcceptFormComponent ]
+    })
+    .compileComponents();
+  }));
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(AcceptFormComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
