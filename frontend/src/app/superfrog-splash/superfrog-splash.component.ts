38addc1bc376c752aa3a951d1808157473b99a1f
jclazalde <joselazalde93@yahoo.com>
2018-11-29 20:35:56 -0600

    SuperFrog landing page

---
+++ frontend/src/app/superfrog-splash/superfrog-splash.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-landing',
+  templateUrl: './superfrog-splash.component.html',
+  styleUrls: ['./superfrog-splash.component.css']
+})
+export class SuperfrogsplashComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
