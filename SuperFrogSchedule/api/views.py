41455e711a344366444eded1be55b254739aaecd
Sarah <allensarahanne@gmail.com>
2019-01-11 16:39:36 -0600

    Update views.py

--- SuperFrogSchedule/api/views.py
+++ SuperFrogSchedule/api/views.py
@@ -1,14 +1,16 @@
 from django.shortcuts import render
 from rest_framework import viewsets, views, generics
-from .models import Superfrog, Admin, Customer, Event, Appearance, OrgType, TeamType
-from .serializers import SuperfrogSerializer, AdminSerializer, CustomerSerializer, EventSerializer, AppearanceSerializer,AppearanceShortSerializer, OrgSerializer, TeamSerializer
+from .models import Superfrog, Admin, Customer, Event, Appearance
+from .serializers import SuperfrogSerializer, AdminSerializer, CustomerSerializer, EventSerializer, AppearanceSerializer,AppearanceShortSerializer
 from rest_framework.permissions import IsAdminUser
 from rest_framework.response import Response
 from rest_framework.decorators import action, list_route
 from django.http import HttpResponse, HttpResponseBadRequest
 from rest_framework.renderers import JSONRenderer
 from rest_framework.parsers import JSONParser
-from django.core.mail import send_mail
+from django.views.decorators.csrf import csrf_exempt
+
+import json
 
 # class AppearanceViewSet(viewsets.ViewSet):
 #     queryset = Appearance.objects.all()
@@ -34,19 +36,34 @@ def list_by_status(request, status=None):
         return HttpResponse(JSONRenderer().render(serializer.data))
     else:
         return HttpResponseBadRequest()
-
+@csrf_exempt
 def appearances(request):
     if request.method == 'GET':
         queryset = Appearance.objects.all()
         serializer = AppearanceShortSerializer(queryset, many=True)
         return HttpResponse(JSONRenderer().render(serializer.data))
     elif request.method=='POST':
-            data = JSONParser().parse(request.body)
-            serializer = AppearanceSerializer(data=data)
-            if serializer.is_valid():
-                serializer.save()
-                return HttpResponse(serializer.data, status = 201)
-            return HttpResponse(serializer.errors, status = 400)
+        data = json.loads(request.body)
+        print(data)
+        appearance_serializer = AppearanceSerializer(data=data['appearance'])
+        if appearance_serializer.is_valid():
+            appearance = appearance_serializer.save()
+            appearance_serializer = AppearanceSerializer(appearance)
+            print(appearance_serializer.data)
+            appearance.save()
+        else:
+            print(appearance_serializer.errors)
+            return HttpResponse(appearance_serializer.errors, status = 400)
+                
+        customer_serializer = CustomerSerializer(data=data['customer'])
+        if customer_serializer.is_valid():
+            customer = customer_serializer.save()
+            customer.save()
+        else:
+            print(customer_serializer.errors)
+            return HttpResponse(appearance_serializer.errors, status = 400)
+        appearance_serializer = AppearanceSerializer(appearance)
+        return HttpResponse(appearance_serializer.data, status = 201)
     else:
         return HttpResponseBadRequest()
 
@@ -60,32 +77,30 @@ def detail(request, id=None):
 
 def create(request):
     if request.method=='POST':
-            data = JSONParser().parse(request.body)
-            serializer = AppearanceSerializer(data=data)
-            if serializer.is_valid():
-                serializer.save()
-                return HttpResponse(serializer.data, status = 201)
-            return HttpResponse(serializer.errors, status = 400)
+        data = JSONParser().parse(request.body)
+        serializer = AppearanceSerializer(data=data)
+        if serializer.is_valid():
+            serializer.save()
+            return HttpResponse(serializer.data, status = 201)
+        return HttpResponse(serializer.errors, status = 400)
     else:
         return HttpResponseBadRequest()
 
-# function to send confirmation to customer. called after customer is saved in db when requesting event.
+# def EmailFunc(em):
 
-def EmailFunc(em):
+# 	recs = Customer.objects.filter(email = em)
+# 	if len(recs) == 0:
+# 		newCustomer = Customer()
+# 		newCustomer.email = em
 
-	recs = Customer.objects.filter(email = em)
-	if len(recs) == 0:
-		newCustomer = Customer()
-		newCustomer.email = em
-
-		send_mail(
-		    'Hay',
-		    'Here is a confirmation message for the event request.',
-		    'superfrog@scheduler.com',
-		    [newCustomer.email],
-		    fail_silently = False,
-		)
-		print("email sent")
-		return True
-	else: 
-		return False
+# 		send_mail(
+# 		    'Hay',
+# 		    'Here is a confirmation message for the event request.',
+# 		    'superfrog@scheduler.com',
+# 		    [newCustomer.email],
+# 		    fail_silently = False,
+# 		)
+# 		print("email sent")
+# 		return True
+# 	else: 
+# 		return False
