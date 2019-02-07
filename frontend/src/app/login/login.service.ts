358cf3c3c02812ba4466d4a640cf5f586362ba42
anhvu <anhvu0897@gmail.com>
2019-02-07 12:12:10 -0600

    Register user works
    Create component and service for login and register
    Register working

--- frontend/src/app/login/login.service.ts
+++ frontend/src/app/login/login.service.ts
@@ -1,9 +1,22 @@
 import { Injectable } from '@angular/core';
+import { HttpHeaders, HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class LoginService {
+  baseurl = "http://127.0.0.1:8000/";
+  //httpHeaders = new HttpHeaders({'Content.Type': 'application/json'});
+  constructor(private http: HttpClient) { }
 
-  constructor() { }
+  registerSuperfrog(userData): Observable<any> {
+    console.log(userData);
+    return this.http.post(this.baseurl + "users/", userData);
+  }
+
+  loginSuperfrog(userData): Observable<any> {
+    console.log(userData);
+    return this.http.post(this.baseurl + "auth/", userData);
+  }
 }
