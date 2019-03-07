e1a77dfc226c5a266f798a8d792aaef04a920065
jclazalde <joselazalde93@yahoo.com>
2019-03-07 12:09:56 -0600

    Demo Prep Work

--- frontend/src/app/accept-reject-details/accept-reject-details.component.ts
+++ frontend/src/app/accept-reject-details/accept-reject-details.component.ts
@@ -1,9 +1,10 @@
 import { Component, OnInit } from '@angular/core';
 import { AdminDetailsService } from './accept-reject-details.service';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
 import { Customer } from '../shared/customer';
 import { Appearance } from '../shared/appearance';
 import { Superfrog } from '../shared/superfrog';
+
 @Component({
   selector: 'app-accept-reject-details',
   templateUrl: './accept-reject-details.component.html',
@@ -13,7 +14,7 @@ export class AcceptRejectDetailsComponent implements OnInit {
   id: number;
   getData: any = {};
   data: {"customer": Customer, "appearance": Appearance, "superfrog": Superfrog} = {"customer":{}, "appearance":{}, "superfrog": {}};
-  constructor(private adminService: AdminDetailsService, private route: ActivatedRoute) { }
+  constructor(private adminService: AdminDetailsService, private route: ActivatedRoute, private router: Router) { }
 
   ngOnInit() {
     this.id = this.route.snapshot.params['id'];
@@ -30,10 +31,12 @@ export class AcceptRejectDetailsComponent implements OnInit {
     this.adminService.acceptAppear(this.id, this.data).subscribe(data => {
       this.getData = data;
     });
+    this.router.navigate(['/list-accept-reject']);
   }
   rejectAppear() {
     this.adminService.rejectAppear(this.id, this.data).subscribe(data => {
       this.getData = data;
     });
+    this.router.navigate(['/list-accept-reject']);
   }
 }
