31efc33253cea2c910bfe4e2d88c9723bc843daa
Sarah <allensarahanne@gmail.com>
2019-04-11 11:33:28 -0500

    new lewk
    adding single nav, new bootstrap, making things look better in general

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -5,7 +5,7 @@ import { LandingComponent } from './landing/landing.component';
 const routes: Routes = [
   {
     path: '',
-    redirectTo: '/home',
+    redirectTo: '/',
     pathMatch: 'full'
   },
   {
